8e26c235ba23e077100a420a32b2d1ef076d3ce2
Karel <karel.fischbein@st.oth-regensburg.de>
2022-05-04 17:08:23 +0200

    completed  unit tests for track models

--- test/fixtures.js
+++ test/fixtures.js
@@ -30,6 +30,10 @@
   export const mozart = {
     title: "Mozart Favourites"
   };
+
+  export const beethoven = {
+    title: "Mozart Favourites"
+  };
   
   export const testPlaylists = [
     {
@@ -40,5 +44,29 @@
     },
     {
       title: "Beethoven Symphonies"
+    }
+  ];
+
+  export const concerto = {
+    "title": "Violin Concerto No. 3",
+    "artist": "Mozart",
+    "duration": 15
+  };
+
+  export const testTracks = [
+    {
+      "title": "Piano Sonata No. 3",
+      "artist": "Beethoven",
+      "duration": 5
+    },
+    {
+      "title": "Piano Sonata No. 7",
+      "artist": "Beethoven",
+      "duration": 6
+    },
+    {
+      "title": "Piano Sonata No. 10",
+      "artist": "Beethoven",
+      "duration": 4
     }
   ];
